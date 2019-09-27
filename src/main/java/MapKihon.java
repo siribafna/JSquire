d649c2324cf7930234f2a1b4bc73739a408f4faf
Siri Bafna <Siri.Bafna@improving.com>
2019-09-27 16:34:04 -0500

    Drills with Files and Maps

--- src/main/java/MapKihon.java
+++ src/main/java/MapKihon.java
@@ -1,5 +1,7 @@
 import framework.MapKihonBase;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,32 +9,38 @@ public class MapKihon extends MapKihonBase {
 
     @Override
     protected Map<String, String> createMapOfStringToString() {
-        throw new UnsupportedOperationException();
+        return new HashMap<String, String>();
     }
 
     @Override
     protected void addKeyAndValueToMap(Map<String, String> inputMap, String key, String value) {
-        throw new UnsupportedOperationException();
+        inputMap.put(key, value);
     }
 
     @Override
     protected String getTheValueAtThisKey(Map<String, String> inputMap, String key) {
-        throw new UnsupportedOperationException();
+        return inputMap.get(key);
     }
 
     @Override
     protected boolean doesMapContainKey(Map<String, String> inputMap, String input) {
-        throw new UnsupportedOperationException();
+        return inputMap.containsKey(input);
     }
 
     @Override
     protected void removeKeyFromMap(Map<String, String> inputMap, String key) {
-        throw new UnsupportedOperationException();
+        inputMap.remove(key);
     }
 
     @Override
     protected int countUniqueWordsInList(List<String> words) {
-        throw new UnsupportedOperationException();
+        Map<String, Integer> map = new HashMap<>();
+        for(var word: words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            }
+        }
+        return map.keySet().size();
     }
 
 }
